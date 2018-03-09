89f52dfe8db14883deb8b550e8b905d0c427ee2b
Dan Heneise <danheneise@me.com>
2018-03-09 12:12:31 -0800

    Add danarchy_couchdb.rb updates.

--- lib/danarchy_couchdb.rb
+++ lib/danarchy_couchdb.rb
@@ -1,4 +1,5 @@
 require_relative 'danarchy_couchdb/version'
+require_relative 'danarchy_couchdb/configuration'
 require 'json'
 require 'net/http'
 
@@ -6,9 +7,9 @@ module DanarchyCouchDB
   class Connection
     def initialize(connection)
       @connection = connection
-      uri = 'http' if !@connection[:use_ssl]
-      uri = 'https' if @connection[:use_ssl]
-      @uri = "#{uri}://#{connection[:host]}:#{connection[:port]}"
+      uri = 'http' if !@connection[:ssl]
+      uri = 'https' if @connection[:ssl]
+      @uri = "#{uri}://#{connection[:hostname]}:#{connection[:port]}"
     end
 
     def get(*args)
@@ -20,6 +21,12 @@ module DanarchyCouchDB
       uri += "/#{document}"        if document
       uri += "/?rev=#{revision}"   if revision
 
+      uri = URI.parse(URI.encode(uri))
+      JSON.parse(request(Net::HTTP::Get.new(uri)), symbolize_names: true)
+    end
+
+    def get_attachment(database, document, attachment)
+      uri  = "#{@uri}/#{database}/#{document}/#{attachment}"
       uri = URI.parse(URI.encode(uri))
       request(Net::HTTP::Get.new(uri))
     end
@@ -36,7 +43,23 @@ module DanarchyCouchDB
       req = Net::HTTP::Put.new(uri)
       req["content-type"] = "application/json"
       req.body = data.to_json if data
-      request(req)
+      JSON.parse(request(req), symbolize_names: true)
+    end
+
+    def put_attachment(*args)
+      database   = args.shift
+      document   = args.shift
+      revision   = args.shift
+      attachment = args.shift
+      data       = args.shift
+      type       = args.shift
+      
+      uri  = "#{@uri}/#{database}/#{document}/#{attachment}?rev=#{revision}"
+      uri = URI.parse(URI.encode(uri))
+      req = Net::HTTP::Put.new(uri)
+      req["content-type"] = "#{type}"
+      req.body = data if data
+      JSON.parse(request(req), symbolize_names: true)
     end
 
     def delete(*args)
@@ -45,29 +68,44 @@ module DanarchyCouchDB
       revision = args.shift
       
       uri  = "#{@uri}/#{database}"
-      uri += "/#{document}"        if document
-      uri += "/?rev=#{revision}"   if revision
+      uri += "/#{document}"      if document
+      uri += "/?rev=#{revision}" if revision
 
       uri = URI.parse(URI.encode(uri))
-      request(Net::HTTP::Delete.new(uri))
+      JSON.parse(request(Net::HTTP::Delete.new(uri)), symbolize_names: true)
+    end
+
+    def delete_attachment(*args)
+      database   = args.shift
+      document   = args.shift
+      revision   = args.shift
+      attachment = args.shift
+      
+      uri  = "#{@uri}/#{database}/#{document}/#{attachment}?rev=#{revision}"
+      uri = URI.parse(URI.encode(uri))
+      JSON.parse(request(Net::HTTP::Delete.new(uri)), symbolize_names: true)
     end
 
     private
     def request(uri)
       response = nil
 
-      Net::HTTP.start(@connection[:host], @connection[:port], :use_ssl => @connection[:use_ssl]) do |http|
-        uri.basic_auth @connection[:user], @connection[:pass]
+      begin
+        Net::HTTP.start(@connection[:hostname], @connection[:port], :use_ssl => @connection[:ssl]) do |http|
+          uri.basic_auth @connection[:username], @connection[:password]
 
-        response = http.request(uri)
+          response = http.request(uri)
 
-        unless response.kind_of?(Net::HTTPSuccess)
-          handle_error(uri, response)
-          break
+          unless response.kind_of?(Net::HTTPSuccess)
+            handle_error(uri, response)
+            break
+          end
         end
+      rescue
+        return "{\"error\":\"Connection Failed\",\"reason\":\"#{uri.uri} is unreachable.\"}\n"
       end
 
-      JSON.parse(response.body, symbolize_names: true)
+      response.body
     end
 
     def handle_error(req, res)
